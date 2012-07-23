b00c4a399dc7d1a6a877d0678eed0886fa41729e
AniCator <contact@anicator.com>
2012-07-23 20:38:24 +0200

    / ‘src/game/client/c_club_dj.cpp’

--- src/game/client/c_club_dj.cpp
+++ src/game/client/c_club_dj.cpp
@@ -1,2 +1,23 @@
 #include "cbase.h"
 #include "bass.h"
+
+CClubDJ::CClubDJ(){
+	Msg("Club DJ\nBy: AniCator & Th13teen\nInitializing...\n");
+
+	HWND hWndPotato = FindWindowA("Valve001", "Project Potato");
+	if(!hWndPotato)
+	{
+		Error("Unable to find Garry's Mod window for BASS library");
+	}
+
+	BOOL bassInit = BASS_Init(-1, 44100, BASS_DEVICE_3D, hWndPotato, NULL);
+	if(!bassInit)
+	{
+		int error = BASS_ErrorGetCode();
+		Msg("BASS Init failed, error code %d\n", error);
+		Error("BASS Init error");
+	}
+	else{
+		Msg("Club DJ's body is ready!\n");
+	}
+}

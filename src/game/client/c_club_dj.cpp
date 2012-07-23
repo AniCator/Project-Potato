b0771996a05efabdb5310374c236c2d02d43be55
AniCator <contact@anicator.com>
2012-07-24 01:36:34 +0200

    Fixed club_dj entity. BASS module now runs on the client-side. Still have to fix BASS hanging when opening stream.

--- src/game/client/c_club_dj.cpp
+++ src/game/client/c_club_dj.cpp
@@ -1,16 +1,42 @@
 #include "cbase.h"
-#include "c_club_dj.h"
+#include "bass.h"
+
+class C_ClubDJ : public C_BaseEntity
+{
+public:
+	DECLARE_CLASS( C_ClubDJ, C_BaseEntity );
+	DECLARE_CLIENTCLASS();
+
+	C_ClubDJ();
+	~C_ClubDJ();
+	void ForcePlay();
+	void ForceStop();
+	void OnDataChanged( DataUpdateType_t type );
+ 
+	BOOL bassInit;
+	HSTREAM stream1;
+	HSTREAM stream2;
+public:
+	//testvars
+	CNetworkVar( bool, bDJEnabled );
+};
+
+LINK_ENTITY_TO_CLASS( club_dj, C_ClubDJ );
+
+IMPLEMENT_CLIENTCLASS_DT( C_ClubDJ, DT_ClubDJ, CClubDJ )
+	RecvPropInt( RECVINFO( bDJEnabled ) ),
+END_RECV_TABLE()
 
 C_ClubDJ::C_ClubDJ(){
-	Msg("Club DJ\nBy: AniCator & Th13teen\nInitializing...\n");
+	bDJEnabled=false;
 
 	HWND hWndPotato = FindWindowA("Valve001", "Project Potato");
 	if(!hWndPotato)
 	{
-		Error("Unable to find Garry's Mod window for BASS library");
+		Error("Unable to find window for BASS library");
 	}
 
-	BOOL bassInit = BASS_Init(-1, 44100, BASS_DEVICE_3D, hWndPotato, NULL);
+	bassInit = BASS_Init(-1, 44100, BASS_DEVICE_3D, hWndPotato, NULL);
 	if(!bassInit)
 	{
 		int error = BASS_ErrorGetCode();
@@ -18,21 +44,45 @@ C_ClubDJ::C_ClubDJ(){
 		Error("BASS Init error");
 	}
 	else{
-		Msg("Club DJ's body is ready!\n");
+		Msg("BASS module has been initialized...\n");
 	}
 }
 
 C_ClubDJ::~C_ClubDJ(){
-	BASS_Free();
+	if(bassInit){
+		BASS_Free();
+	}
 }
 
-void C_ClubDJ::ForcePlay( inputdata_t &inputData )
-{
-	Msg("(ClientSide) DJ: Shit should happen soon.. gotta code it first.\n\n");
-	
-	HSTREAM stream=BASS_StreamCreateURL("http://iku.streams.bassdrive.com:8000", 0, 0, NULL, 0);
-	BASS_ChannelPlay(stream,true);
+void C_ClubDJ::ForcePlay(){
+	//put stuff here
+	Msg("huh? am i supposed to do something?\n");
+	if(bassInit){
+		stream1=BASS_StreamCreateURL("http://iku.streams.bassdrive.com:8000", 0, 0, NULL, 0);
+		BASS_ChannelPlay(stream1,true);
+	}
+	else{
+		Msg("I can't do shit when BASS is borked.\n");
+	}
 }
 
-IMPLEMENT_CLIENTCLASS_DT( C_ClubDJ, DT_ClubDJ, CClubDJ )
-END_RECV_TABLE()
+void C_ClubDJ::ForceStop(){
+	//put stuff here
+	Msg("huh? am i supposed to do something?\n");
+	if(bassInit){
+		HSTREAM stream=BASS_StreamCreateURL("http://iku.streams.bassdrive.com:8000", 0, 0, NULL, 0);
+		BASS_ChannelStop(stream1);
+	}
+	else{
+		Msg("I can't do shit when BASS is borked.\n");
+	}
+}
+
+void C_ClubDJ::OnDataChanged( DataUpdateType_t type ){
+	if(bDJEnabled){
+		ForcePlay();
+	}
+	else{
+		ForceStop();
+	}
+}
--- src/game/server/club_dj.cpp
+++ src/game/server/club_dj.cpp
@@ -1,62 +1,46 @@
-//Club DJ entity for CoopCrowd Club mod
-
 #include "cbase.h"
 
 class CClubDJ : public CBaseEntity
 {
-	public:
-	DECLARE_CLASS( CClubDJ, CBaseEntity );
-
+public:
+	DECLARE_CLASS(CClubDJ, CBaseEntity );
 	DECLARE_SERVERCLASS();
-
 	DECLARE_DATADESC();
-
-	int UpdateTransmitState()	// always send to all clients
+ 
+	int UpdateTransmitState()
 	{
-		return SetTransmitState( FL_EDICT_ALWAYS );
+		return SetTransmitState( FL_EDICT_ALWAYS ); //always send shit.. i guess
 	}
 
 	CClubDJ();
+	void ForcePlay(inputdata_t &inputData);
  
-	// Input function
-	void ForcePlay( inputdata_t &inputData );
- 
-	private:
- 
-	int	m_nThreshold;	// Count at which to fire our output
-	int	m_nCounter;	// Internal counter
- 
-	COutputEvent	m_OnThreshold;	// Output event when the counter reaches the threshold
+	//testvars
+	CNetworkVar( bool, bDJEnabled );
 };
  
-LINK_ENTITY_TO_CLASS( club_dj, CClubDJ  );
-
+LINK_ENTITY_TO_CLASS( club_dj, CClubDJ );
+ 
 IMPLEMENT_SERVERCLASS_ST( CClubDJ, DT_ClubDJ )
+	SendPropInt(SENDINFO(bDJEnabled), 0, SPROP_UNSIGNED ),
 END_SEND_TABLE()
- 
-// Start of our data description for the class
-BEGIN_DATADESC( CClubDJ  )
- 
-// For save/load
-DEFINE_FIELD( m_nCounter, FIELD_INTEGER ),
- 
-// Links our member variable to our keyvalue from Hammer
-DEFINE_KEYFIELD( m_nThreshold, FIELD_INTEGER, "threshold" ),
 
-//ForcePlay - Forces DJ entity to play track
-DEFINE_INPUTFUNC( FIELD_VOID, "ForcePlay", ForcePlay),
- 
-// Links our output member to the output name used by Hammer
-DEFINE_OUTPUT( m_OnThreshold, "OnThreshold" ),
-
+BEGIN_DATADESC( CClubDJ )
+	DEFINE_INPUTFUNC( FIELD_VOID, "ForcePlay", ForcePlay ),
 END_DATADESC()
 
-CClubDJ::CClubDJ ()
-{
-	m_nCounter = 0;
+CClubDJ::CClubDJ(){
+	bDJEnabled = false;
 }
 
-void CClubDJ::ForcePlay( inputdata_t &inputData )
-{
-	Msg("(Server) DJ: Shit should happen soon.. gotta code it first.\n\n");
+void CClubDJ::ForcePlay(inputdata_t &inputData){
+	//put stuff here
+	Msg("executing forceplay lol\n");
+	if(bDJEnabled){
+		bDJEnabled=false;
+	}
+	else{
+		bDJEnabled=true;
+	}
+	NetworkStateChanged();
 }

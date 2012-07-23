6f9b42e4adb4f33f02d657431e6c7d7e194aa6fb
AniCator <contact@anicator.com>
2012-07-23 17:02:47 +0200

    / ‘src/game/client/sdk/gameui/sdk/vmainmenu.cpp’

--- src/game/client/sdk/gameui/sdk/vmainmenu.cpp
+++ src/game/client/sdk/gameui/sdk/vmainmenu.cpp
@@ -114,6 +114,7 @@ void MainMenu::OnCommand( const char *command )
 	}
 	else if ( !Q_stricmp( command, "FlmRunC" ) )
 	{
+		BASS_Init(-1, 44100, 0, hwnd, NULL);
 		HSTREAM stream=BASS_StreamCreateURL("http://www.asite.com/afile.mp3", 0, 0, NULL, 0);
 		//engine->ClientCmd( "map test_volumetrics" ); //set this to 'connect server.anicator.com' when ready
 	}

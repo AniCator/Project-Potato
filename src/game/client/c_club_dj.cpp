0166e578711377dbf71f491f9fc9eef0a0fdacc3
AniCator <contact@anicator.com>
2012-07-23 20:52:55 +0200

    / ‘src/game/client/c_club_dj.cpp’

--- src/game/client/c_club_dj.cpp
+++ src/game/client/c_club_dj.cpp
@@ -5,7 +5,6 @@ class C_ClubDJ : public C_BaseEntity
 {
 	public:
 	DECLARE_CLASS( C_ClubDJ, C_BaseEntity );
-	DECLARE_DATADESC();
 	DECLARE_CLIENTCLASS();
 
 	C_ClubDJ();
@@ -14,34 +13,12 @@ class C_ClubDJ : public C_BaseEntity
 	void ForcePlay( inputdata_t &inputData );
  
 	private:
- 
-	int	m_nThreshold;	// Count at which to fire our output
-	int	m_nCounter;	// Internal counter
- 
-	COutputEvent	m_OnThreshold;	// Output event when the counter reaches the threshold
 };
  
 LINK_ENTITY_TO_CLASS( club_dj, C_ClubDJ  );
 
 IMPLEMENT_CLIENTCLASS_DT( C_ClubDJ, DT_ClubDJ, CClubDJ )
 END_RECV_TABLE()
- 
-// Start of our data description for the class
-BEGIN_DATADESC( C_ClubDJ  )
- 
-// For save/load
-DEFINE_FIELD( m_nCounter, FIELD_INTEGER ),
- 
-// Links our member variable to our keyvalue from Hammer
-DEFINE_KEYFIELD( m_nThreshold, FIELD_INTEGER, "threshold" ),
-
-//ForcePlay - Forces DJ entity to play track
-DEFINE_INPUTFUNC( FIELD_VOID, "ForcePlay", ForcePlay),
- 
-// Links our output member to the output name used by Hammer
-DEFINE_OUTPUT( m_OnThreshold, "OnThreshold" ),
- 
-END_DATADESC()
 
 C_ClubDJ::C_ClubDJ(){
 	Msg("Club DJ\nBy: AniCator & Th13teen\nInitializing...\n");

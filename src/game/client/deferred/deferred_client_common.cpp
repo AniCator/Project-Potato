b3bbe4b9d8565995125e0502a54bfc9b0c0c118f
AniCator <contact@anicator.com>
2012-07-23 15:07:13 +0200

    + ‘src/dx9sdk/utilities/dx_proxy.dll’ + ‘src/dx9sdk/utilities/fxc.exe’ + ‘src/dx9sdk/utilities/mysql_wrapper.dll’ + ‘src/dx9sdk/utilities/psa.exe’ + ‘src/dx9sdk/utilities/vsa.exe’ + ‘src/engine/iblackbox.h’ + ‘src/game/client/AnimateSpecificTextureProxy.cpp’ + ‘src/game/client/C_MaterialModifyControl.cpp’ + ‘src/game/client/C_WaterLODControl.cpp’ + ‘src/game/client/Debug_swarm/AnimateSpecificTextureProxy.obj’ + ‘src/game/client/Debug_swarm/CDefLight.obj’ + ‘src/game/client/Debug_swarm/CDefLightContainer.obj’ + ‘src/game/client/Debug_swarm/CDefLightGlobal.obj’ + ‘src/game/client/Debug_swarm/CL.read.1.tlog’ + ‘src/game/client/Debug_swarm/CL.write.1.tlog’ + ‘src/game/client/Debug_swarm/C_Func_Monitor.obj’ + ‘src/game/client/Debug_swarm/C_MaterialModifyControl.obj’ + ‘src/game/client/Debug_swarm/C_WaterLODControl.obj’ + ‘src/game/client/Debug_swarm/ClientScoreBoardDialog.obj’ + ‘src/game/client/Debug_swarm/DefCookieProjectable.obj’ ...

---
+++ src/game/client/deferred/deferred_client_common.cpp
@@ -0,0 +1,46 @@
+
+#include "cbase.h"
+#include "deferred/deferred_shared_common.h"
+
+ConVar deferred_rt_shadowspot_res( "deferred_rt_shadowspot_res", "1024", FCVAR_HIDDEN );
+ConVar deferred_rt_shadowpoint_res( "deferred_rt_shadowpoint_res", "1024", FCVAR_HIDDEN );
+
+ConVar deferred_lightmanager_debug( "deferred_lightmanager_debug", "0" );
+
+ConVar deferred_override_globalLight_enable( "deferred_override_globalLight_enable", "0" );
+ConVar deferred_override_globalLight_shadow_enable( "deferred_override_globalLight_shadow_enable", "1" );
+ConVar deferred_override_globalLight_diffuse( "deferred_override_globalLight_diffuse", "1 1 1" );
+ConVar deferred_override_globalLight_ambient_high( "deferred_override_globalLight_ambient_high", "0 0.04 0.08" );
+ConVar deferred_override_globalLight_ambient_low( "deferred_override_globalLight_ambient_low", "0 0.07 0.09" );
+
+ConVar deferred_radiosity_enable( "deferred_radiosity_enable", "1" );
+ConVar deferred_radiosity_propagate_count( "deferred_radiosity_propagate_count", "1" );
+ConVar deferred_radiosity_propagate_count_far( "deferred_radiosity_propagate_count_far", "0" );
+ConVar deferred_radiosity_blur_count( "deferred_radiosity_blur_count", "2" );
+ConVar deferred_radiosity_blur_count_far( "deferred_radiosity_blur_count_far", "1" );
+ConVar deferred_radiosity_debug( "deferred_radiosity_debug", "0" );
+
+void OnCookieTableChanged( void *object, INetworkStringTable *stringTable, int stringNumber, const char *newString, void const *newData )
+{
+	if ( !newString || Q_strlen( newString ) < 1 )
+		return;
+
+	GetLightingManager()->OnCookieStringReceived( newString, stringNumber );
+}
+
+void CalcBoundaries( Vector *list, const int &num, Vector &min, Vector &max )
+{
+	Assert( num > 0 );
+
+	min = *list;
+	max = *list;
+
+	for ( int i = 1; i < num; i++ )
+	{
+		for ( int x = 0; x < 3; x++ )
+		{
+			min[ x ] = MIN( min[ x ], list[ i ][ x ] );
+			max[ x ] = MAX( max[ x ], list[ i ][ x ] );
+		}
+	}
+}
---
+++ src/materialsystem/swarmshaders/deferred_utility.cpp
@@ -0,0 +1,36 @@
+
+#include "deferred_includes.h"
+
+#include "tier0/memdbgon.h"
+
+ConVar deferred_radiosity_enable( "deferred_radiosity_enable", "1" ); // from client.dll
+ConVar deferred_radiosity_multiplier( "deferred_radiosity_multiplier", "0.4" );
+
+void GetTexcoordSettings( const bool bDecal, const bool bMultiBlend,
+	int &iNumTexcoords, int **iTexcoordDim )
+{
+	static int iDimDefault[] = {
+		2, 0, 3,
+	};
+
+	static int iDimMultiBlend[] = {
+		2, 2, 0, 4,
+		4, 4, 4, 4,
+	};
+
+	if ( bMultiBlend )
+	{
+		*iTexcoordDim = iDimMultiBlend;
+		iNumTexcoords = 8;
+	}
+	else if ( bDecal )
+	{
+		*iTexcoordDim = iDimDefault;
+		iNumTexcoords = 3;
+	}
+	else
+	{
+		*iTexcoordDim = iDimDefault;
+		iNumTexcoords = 1;
+	}
+}
